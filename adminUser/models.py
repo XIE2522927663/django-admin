6c1127b36561e6dbb574e897ce01fd4745bacb32
XIE7654 <2522927663@qq.com>
2018-08-24 15:21:48 +0800

    用户列表

--- adminUser/models.py
+++ adminUser/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class adminUser (models.Model):
+    TYPE = (
+        ('A', '管理员'),
+        ('B', '普通用户')
+    )
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    creatDate = models.DateTimeField('创建时间', default=now)
+    activation = models.BooleanField('是否激活', default=True)
+    type = models.CharField('类型', max_length=1, choices=TYPE, default='B')
+
+    def __str__(self):
+        return self.name
